d6ab7f6f29f9ac3a87eaac92af7cd550c3e8027f
lolddong <w2crosss@gmail.com>
2025-06-25 14:18:07 +0900

    update 11_SPRING(basic)

--- 08_SPRING/11_SPRING(basic)/apiserver/src/main/java/org/scoula/security/config/SecurityConfig.java
+++ 08_SPRING/11_SPRING(basic)/apiserver/src/main/java/org/scoula/security/config/SecurityConfig.java
@@ -132,5 +132,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .userDetailsService(userDetailsService)
                 .passwordEncoder(passwordEncoder());
     }
-
 }
